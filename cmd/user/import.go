a1b9945662b7b41237d2892e2a1c00e7b563983e
srz_zumix <zumix.cpp@gmail.com>
2026-03-21 08:36:18 +0900

    add user import (#122)

---
+++ cmd/user/import.go
@@ -0,0 +1,81 @@
+package user
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/srz-zumix/go-gh-extension/pkg/gh"
+	"github.com/srz-zumix/go-gh-extension/pkg/ioutil"
+	"github.com/srz-zumix/go-gh-extension/pkg/logger"
+	"github.com/srz-zumix/go-gh-extension/pkg/parser"
+)
+
+// NewImportCmd creates a new cobra.Command for importing users into an organization.
+func NewImportCmd() *cobra.Command {
+	var owner string
+	var dryrun bool
+	var defaultRole string
+
+	cmd := &cobra.Command{
+		Use:   "import <input>",
+		Short: "Import users into the organization",
+		Long: `Read a JSON list of users (as produced by 'user list --format json') and add each user to the organization.
+Entries without a "login" field are skipped. The role is taken from the "role_name" field if present; otherwise --role is used as default.
+Specify '-' as input to read from stdin.`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			input := args[0]
+
+			repository, err := parser.Repository(parser.RepositoryOwner(owner))
+			if err != nil {
+				return fmt.Errorf("error parsing repository: %w", err)
+			}
+
+			users, err := ioutil.DecodeJSONFile[[]*gh.GitHubUser](input)
+			if err != nil {
+				return fmt.Errorf("error reading input: %w", err)
+			}
+
+			if dryrun {
+				logger.Info("Dry run completed. No changes were made.", "count", len(users))
+				return nil
+			}
+
+			ctx := context.Background()
+			client, err := gh.NewGitHubClientWithRepo(repository)
+			if err != nil {
+				return fmt.Errorf("error creating GitHub client: %w", err)
+			}
+
+			var errs []error
+			for _, u := range users {
+				if u.Login == nil {
+					continue
+				}
+				role := u.GetRoleName()
+				if role == "" {
+					role = defaultRole
+				}
+				_, err := gh.AddOrUpdateOrgMember(ctx, client, repository, *u.Login, role)
+				if err != nil {
+					errs = append(errs, fmt.Errorf("failed to add user '%s': %w", *u.Login, err))
+				} else {
+					logger.Info("User added to organization.", "username", *u.Login, "role", role)
+				}
+			}
+
+			if len(errs) > 0 {
+				return fmt.Errorf("encountered errors during import: %v", errs)
+			}
+			return nil
+		},
+	}
+
+	f := cmd.Flags()
+	f.StringVar(&owner, "owner", "", "Specify the organization name")
+	f.BoolVarP(&dryrun, "dryrun", "n", false, "Dry run: do not actually apply changes")
+	f.StringVar(&defaultRole, "role", gh.TeamMembershipRoleMember, "Default role when not specified in input (member or admin)")
+
+	return cmd
+}
