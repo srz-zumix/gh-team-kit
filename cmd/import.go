9be513320b8f7b842c9d602bec892ef3cc3bdcb1
srz_zumix <zumix.cpp@gmail.com>
2026-04-17 11:26:03 +0900

    fix usermap capture (#152)

--- cmd/import.go
+++ cmd/import.go
@@ -33,6 +33,8 @@ func NewImportCmd() *cobra.Command {
 		Use:   "import <input>",
 		Short: "Import team information",
 		Long: `Read and apply team information to the specified organization.
+
+<input>: Path to a YAML configuration file produced by 'export', or '-' to read from standard input.
 When --usermap is specified, source logins are automatically converted to target logins using the mapping file (as produced by 'user map').`,
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
